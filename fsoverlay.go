13dfec359bbf9cbc874fda99104e02ecd609f1c9
Michael Woolnough <michael.woolnough@gmail.com>
2024-06-09 19:02:16 +0100

    added method to implement fs.ReadFileFS

--- fsoverlay.go
+++ fsoverlay.go
@@ -25,3 +25,22 @@ func (o Overlay) Open(path string) (fs.File, error) {
 
 	return nil, firstError
 }
+
+func (o Overlay) ReadFile(name string) ([]byte, error) {
+	var firstError error
+
+	for _, ofs := range o {
+		data, err := fs.ReadFile(ofs, name)
+		if errors.Is(err, fs.ErrNotExist) {
+			if firstError == nil {
+				firstError = err
+			}
+
+			continue
+		}
+
+		return data, err
+	}
+
+	return nil, firstError
+}
