aacaf1f005a328baea76cedc5439a492a8c03e08
Michael Woolnough <michael.woolnough@gmail.com>
2024-06-10 20:59:45 +0100

    add LStat method for OS FS

--- os.go
+++ os.go
@@ -1,8 +1,10 @@
 package fsoverlay
 
 import (
+	"errors"
 	"io/fs"
 	"os"
+	"path"
 )
 
 type OS string
@@ -22,3 +24,38 @@ func (o OS) ReadDir(name string) ([]fs.DirEntry, error) {
 func (o OS) Stat(name string) (fs.FileInfo, error) {
 	return os.DirFS(string(o)).(fs.StatFS).Stat(name)
 }
+
+func (o OS) LStat(name string) (fs.FileInfo, error) {
+	pname, err := join(string(o), name)
+	if err != nil {
+		return nil, &fs.PathError{
+			Op:   "lstat",
+			Path: name,
+			Err:  err,
+		}
+	}
+
+	fi, err := os.Lstat(pname)
+	if err != nil {
+		err.(*os.PathError).Path = name
+
+		return nil, err
+	}
+
+	return fi, nil
+}
+
+func join(base, name string) (string, error) {
+	if base == "" {
+		return "", ErrEmptyRoot
+	}
+
+	combined := path.Join(base, name)
+	if !fs.ValidPath(combined) {
+		return "", fs.ErrInvalid
+	}
+
+	return combined, nil
+}
+
+var ErrEmptyRoot = errors.New("invalid root directory")
